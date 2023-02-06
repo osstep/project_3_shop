975de2396d069b16ea225ebf6ef40ad21d7eda76
Stepan Osadsi <osstep3@gmail.com>
2023-02-06 14:29:57 +0300

    layout of the first block

--- gulpfile.js
+++ gulpfile.js
@@ -15,16 +15,13 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function(){
-    return gulp.src('src/sass/*.+(scss|sass)')
+    return gulp.src('src/sass/**/*.+(scss|sass)')
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({
                 prefix: "",
                 suffix: ".min",
             }))
-            .pipe(autoprefixer({
-                
-                cascade: false
-            }))
+            .pipe(autoprefixer())
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest('src/css'))
             .pipe(browserSync.stream());
@@ -32,7 +29,7 @@ gulp.task('styles', function(){
 
 
 gulp.task('watch', function(){
-    gulp.watch('src/sass/*.+(scss|sass)', gulp.parallel('styles'))
+    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'))
     gulp.watch('src/*html').on('change', browserSync.reload)
 });
 
