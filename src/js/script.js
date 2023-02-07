6458e616a21803354534be6ee0fb29f8246a7eac
Stepan Osadsi <osstep3@gmail.com>
2023-02-07 16:59:41 +0300

    catalog anumation

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"></button>',
         responsive: [
@@ -17,3 +17,16 @@ $(document).ready(function(){
           ]
       });
   });
+
+document.querySelectorAll(".catalog-item__link").forEach((item) => {
+    item.addEventListener('click', catalogItemHidden)
+})
+
+
+
+function catalogItemHidden(item) {
+    item.preventDefault();
+    let catalogItem = this.closest('.catalog-item');
+    catalogItem.querySelector('.catalog-item__content').classList.toggle('catalog-item_hidden');
+    catalogItem.querySelector('.catalog-item__list').classList.toggle('catalog-item_hidden');
+}   
